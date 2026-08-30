c9108af73b11e444db3f4e6a960ad2eff0028e2c
Fatemeh <mehreganfatemeh903@gmail.com>
2026-08-30 16:12:56 +0330

    fix: serve media files on railway

--- Myproject/urls.py
+++ Myproject/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .seo import robots_txt
 from django.contrib.sitemaps.views import sitemap
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 from store.sitemaps import StaticViewSitemap, ProductSitemap
 
@@ -28,11 +28,12 @@ urlpatterns = [
     path('user_dashboard/', include('user_dashboard.urls')),
 
     # ================= SEO =================
-path(
-    'robots.txt',
-    robots_txt,
-    name='robots_txt'
-),
+    path(
+        'robots.txt',
+        robots_txt,
+        name='robots_txt'
+    ),
+
     path(
         'sitemap.xml',
         sitemap,
@@ -42,7 +43,10 @@ path(
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
