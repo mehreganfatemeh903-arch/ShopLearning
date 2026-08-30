b3a80e77012b13b5b76f6e908319482efe8f379c
Fatemeh <mehreganfatemeh903@gmail.com>
2026-08-30 16:02:45 +0330

    fix: serve media files in production

--- Myproject/urls.py
+++ Myproject/urls.py
@@ -42,8 +42,7 @@ path(
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
